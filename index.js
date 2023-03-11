019f2985967dee949d466afff03477e84a7434f6
oderomboya <davismboya3@gmail.com>
2023-03-11 22:20:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  
+  const names = ['Guadalupe', 'Ollie', 'Aki'];
+  const event = 'surprise'
+  function writeCards(names, event) {
+    const birthdayMessage = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      names.push(message);
+    }
+    return birthdayMessage;
+  }
+  
+  
